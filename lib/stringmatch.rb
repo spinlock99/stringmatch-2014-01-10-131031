1067d280f1b4fc77ccd758c654cf446f3ebee542
Andrew Dixon <andrew.dixon@twitch.tv>
2014-02-28 15:37:49 -0800

    kata

--- lib/stringmatch.rb
+++ lib/stringmatch.rb
@@ -1,2 +1,18 @@
-class Stringmatch
+class StringMatch
+  attr_accessor :base_string
+
+  def base_string=(base_string='')
+    raise "Argument must be of type String" unless base_string.class == String
+
+    @base_string = base_string
+  end
+
+  alias :initialize :base_string=
+
+  def match match_string
+    matched_characters = @base_string.split(//).collect do |character|
+      character if match_string.include?(character)
+    end
+    matched_characters.join.squeeze
+  end
 end
--- spec/stringmatch_spec.rb
+++ spec/stringmatch_spec.rb
@@ -1,12 +1,42 @@
 require 'spec_helper'
 require 'stringmatch'
 
-describe Stringmatch do
+describe StringMatch do
+  let(:base_string) { "hello world" }
+  let(:string_match) { StringMatch.new(base_string) }
+
+  shared_examples "initialize" do
+    specify { expect { subject }.to_not raise_exception }
+  end
+
   describe "new" do
-    it "should instantiate" do
-      lambda {
-        Stringmatch.new
-      }.should_not raise_exception
+    subject { StringMatch.new(base_string) }
+    it_behaves_like "initialize"
+
+    context "empty initialization" do
+      subject { StringMatch.new }
+      it_behaves_like "initialize"
     end
+  end
+
+  describe ".base_string" do
+    let(:things) { "things" }
+
+    subject { string_match.base_string }
+
+    it { should eq(base_string) }
+
+    it "resets the base string" do
+      string_match.base_string = things
+      subject.should eq(things)
+    end
+  end
+
+  describe ".match" do
+    let(:base_string) { "hello" }
+    let(:match_string) { "example" }
+    subject { string_match.match(match_string) }
+    specify { expect { subject }.to_not raise_error }
+    it { should eq("el") }
   end
 end
